5fceb16587bccecf50ddcddf3572561df703e715
Jose Mejia <josemejia91198@gmail.com>
2022-06-25 22:35:55 -0700

    first version of PlayRound

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ function computerPlay(){
 }
 
 // computerPlay();
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "scissors"){
+        return `You win! ${playerSelection} beats ${computerSelection}!`;
+    }else if(playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "paper") {
+        return `You lose! ${computerSelection} beats ${playerSelection}!`
+    }else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "rock"){
+        return `Draw!`
+    }
+
+    if(playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "scissors"){
+        return `You lose! ${computerSelection} beats ${playerSelection}!`
+    }else if(playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "paper") {
+        return `Draw!`
+    }else if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "rock"){
+        return `You win! ${playerSelection} beats ${computerSelection}!`;
+    }
+
+    if(playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "scissors"){
+        return `Draw!`
+    }else if(playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "paper") {
+        return `You win! ${playerSelection} beats ${computerSelection}!`;
+    }else if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "rock"){
+        return `You lose! ${computerSelection} beats ${playerSelection}!`
+    }
+}
