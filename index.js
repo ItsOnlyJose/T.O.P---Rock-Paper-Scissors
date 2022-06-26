d0cadd4731753f72dcd7f3ec0e5d4a2a037ba3e9
Jose Mejia <josemejia91198@gmail.com>
2022-06-25 22:44:38 -0700

    updated if else statements, much cleaner

--- index.js
+++ index.js
@@ -6,28 +6,58 @@ function computerPlay(){
 
 // computerPlay();
 
-function playRound(playerSelection, computerSelection) {
-    if(playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "scissors"){
-        return `You win! ${playerSelection} beats ${computerSelection}!`;
-    }else if(playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "paper") {
-        return `You lose! ${computerSelection} beats ${playerSelection}!`
-    }else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "rock"){
-        return `Draw!`
+// function playRound(playerSelection, computerSelection) {
+//     if(playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "scissors"){
+//         return `You win! ${playerSelection} beats ${computerSelection}!`;
+//     }else if(playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "paper") {
+//         return `You lose! ${computerSelection} beats ${playerSelection}!`
+//     }else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "rock"){
+//         return `Draw!`
+//     }
+
+//     if(playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "scissors"){
+//         return `You lose! ${computerSelection} beats ${playerSelection}!`
+//     }else if(playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "paper") {
+//         return `Draw!`
+//     }else if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "rock"){
+//         return `You win! ${playerSelection} beats ${computerSelection}!`;
+//     }
+
+//     if(playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "scissors"){
+//         return `Draw!`
+//     }else if(playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "paper") {
+//         return `You win! ${playerSelection} beats ${computerSelection}!`;
+//     }else if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "rock"){
+//         return `You lose! ${computerSelection} beats ${playerSelection}!`
+//     }
+// }
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection.toLowerCase() == "rock"){
+        if(computerSelection.toLowerCase() == "scissors"){
+            return `You win! ${playerSelection} beats ${computerSelection}!`;
+        }else if(computerSelection.toLowerCase() == "paper"){
+            return `You lose! ${computerSelection} beats ${playerSelection}!`
+        }  
     }
 
-    if(playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "scissors"){
-        return `You lose! ${computerSelection} beats ${playerSelection}!`
-    }else if(playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "paper") {
-        return `Draw!`
-    }else if (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "rock"){
-        return `You win! ${playerSelection} beats ${computerSelection}!`;
+    if(playerSelection.toLowerCase() == "paper"){
+        if(computerSelection.toLowerCase() == "rock"){
+            return `You win! ${playerSelection} beats ${computerSelection}!`;
+        }else if(computerSelection.toLowerCase() == "scissors"){
+            return `You lose! ${computerSelection} beats ${playerSelection}!`
+        }  
+    }
+    
+    if(playerSelection.toLowerCase() == "scissors"){
+        if(computerSelection.toLowerCase() == "paper"){
+            return `You win! ${playerSelection} beats ${computerSelection}!`;
+        }else if(computerSelection.toLowerCase() == "rock"){
+            return `You lose! ${computerSelection} beats ${playerSelection}!`
+        }  
     }
 
-    if(playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "scissors"){
+    if(playerSelection.toLowerCase() == computerSelection.toLowerCase()){
         return `Draw!`
-    }else if(playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "paper") {
-        return `You win! ${playerSelection} beats ${computerSelection}!`;
-    }else if (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "rock"){
-        return `You lose! ${computerSelection} beats ${playerSelection}!`
     }
 }
